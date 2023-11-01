f0d7d3fe1566a7e446a7102d428148e7fde6c7b8
azimkubatov <kubat.azim@gmail.com>
2023-11-01 22:42:14 +0600

    example 1

--- bin/example1.dart
+++ bin/example1.dart
@@ -1,3 +1,19 @@
 void main() {
-  print('kjdlkajsf');
+  final double? doubleValue = eitherIntOrDouble();
+  print(doubleValue);
+
+  final int? intValue = eitherIntOrDouble();
+  print(intValue);
+}
+
+T? eitherIntOrDouble<T extends num>() {
+  switch (T) {
+    case int:
+      return 1 as T;
+
+    case double:
+      return 1.1 as T;
+    default:
+      return null;
+  }
 }
